4b5c6b26716f00a9ddd6ccee96982bfe2f240a15
Lase <lase_star@mail.ru>
2023-03-23 19:03:58 +0300

    Add_dz_7

--- client.py
+++ client.py
@@ -8,7 +8,7 @@
     ○ port — tcp-порт на сервере, по умолчанию 7777.
 """
 
-from socket import *
+import socket
 import time
 import json
 import sys
@@ -58,36 +58,79 @@ def parse_message(msg_pm):
         return f'{msg_pm["response"]} : {msg_pm["alert"]}'
     raise ValueError
 
+@log
+def create_message():
+    message = input('Введите сообщение для отправки или \'!!!\' для завершения работы: ')
+    if message == '!!!':
+        CLIENT_LOGGER.info('Завершение работы по команде пользователя.')
+        print('Спасибо за использование нашего сервиса!')
+        sys.exit(0)
+    m = {
+        "action": "message",
+        "time": time.time(),
+        "user": {
+            "account_name": "User1",
+            "password": "Password123"
+        },
+        "message_text": message
+    }
+    CLIENT_LOGGER.debug(f'Сформирован словарь сообщения: {m}')
+    return m
 
 def main():
     try:
         server_address = sys.argv[1]
         server_port = int(sys.argv[2])
+        client_mode = sys.argv[3]
         if server_port < 1024 or server_port > 65535:
             raise ValueError
     except IndexError:
         server_address = 'localhost'
         server_port = 7777
+        client_mode = 'send'
     except ValueError:
         CLIENT_LOGGER.critical(f'Попытка запуска клиента с указанием неподходящего порта {server_port}.')
         sys.exit(1)
 
-    s = socket(AF_INET, SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_address, server_port))
     CLIENT_LOGGER.info(f'Запущен клиент, порт для подключений: {server_port}, '
                        f'адрес сервера: {server_address}. ')
 
-    msg = create_presence()
-    CLIENT_LOGGER.info(f'Cформировано сообщение серверу {msg}')
-    send_message(msg, s)
-    CLIENT_LOGGER.debug(f'Сообщение отправлено на сервер {s}')
+    if client_mode == 'send':
+        print('SENDER')
+    else:
+        print('GETER')
+    while True:
+        if client_mode == 'send':
+            try:
+                msg = create_message()
+                send_message(msg, s)
+            except:
+                CLIENT_LOGGER.error(f'Соединение с сервером {server_address} было потеряно.')
+                sys.exit(1)
 
-    try:
-        msg_from_server = get_message(s)
-        answer = parse_message(msg_from_server)
-        CLIENT_LOGGER.info(f'Принят ответ от сервера {answer}')
-    except (ValueError, json.JSONDecodeError):
-        CLIENT_LOGGER.error(f'Не удалось декодировать сообщение сервера.')
+        if client_mode == 'listen':
+            try:
+                msg_from_server = get_message(s)
+                answer = parse_message(msg_from_server)
+                print(answer)
+                CLIENT_LOGGER.info(f'Принят ответ от сервера {answer}')
+            except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                CLIENT_LOGGER.error(f'Соединение с сервером {server_address} было потеряно.')
+                sys.exit(1)
+
+    # msg = create_presence()
+    # CLIENT_LOGGER.info(f'Cформировано сообщение серверу {msg}')
+    # send_message(msg, s)
+    # CLIENT_LOGGER.debug(f'Сообщение отправлено на сервер {s}')
+    #
+    # try:
+    #     msg_from_server = get_message(s)
+    #     answer = parse_message(msg_from_server)
+    #     CLIENT_LOGGER.info(f'Принят ответ от сервера {answer}')
+    # except (ValueError, json.JSONDecodeError):
+    #     CLIENT_LOGGER.error(f'Не удалось декодировать сообщение сервера.')
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -8,11 +8,14 @@
     доступные адреса).
 """
 import logging
+import time
+
 import log.server_log_config
 from socket import *
 import json
 import sys
 from decorators import log
+import select
 
 # Параметры логирования
 SERVER_LOGGER = logging.getLogger('server')
@@ -31,7 +34,7 @@ def get_message(client):
 
 
 @log
-def client_response_check(msg_crch):
+def client_response_check(msg_crch, messages_list):
     SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {msg_crch}')
     if 'action' not in msg_crch \
             or 'time' not in msg_crch \
@@ -43,11 +46,11 @@ def client_response_check(msg_crch):
             'alert': 'Bad Request'
         }
 
-    if msg_crch['action'] != 'authenticate':
-        return {
-            'response': 401,
-            'alert': 'Not authorized'
-        }
+    # if msg_crch['action'] != 'authenticate':
+    #     return {
+    #         'response': 401,
+    #         'alert': 'Not authorized'
+    #     }
 
     if msg_crch['user']['account_name'] != 'User1' \
             or msg_crch['user']['password'] != 'Password123':
@@ -56,10 +59,19 @@ def client_response_check(msg_crch):
             'alert': 'wrong username/password'
         }
 
-    return {
-        'response': 200,
-        'alert': 'OK'
-    }
+    if msg_crch['action'] == 'authenticate':
+        return {
+            'response': 200,
+            'alert': 'OK'
+        }
+    elif msg_crch['action'] == 'message':
+        messages_list.append((msg_crch['user']['account_name'], msg_crch['mess_text']))
+        return
+    else:
+        return {
+            'response': 401,
+            'alert': 'Not authorized'
+        }
 
 
 @log
@@ -67,6 +79,18 @@ def create_client_presence(msg_ccp, client_ccp):
     js_msg = json.dumps(msg_ccp)
     encod_js_msg = js_msg.encode('utf-8')
     client_ccp.send(encod_js_msg)
+
+
+# @log
+# def process_client_message(message, messages_lst, client):
+#     """
+#     :param message:
+#     :param messages_lst:
+#     :param client:
+#     :return:
+#     """
+#
+#     SERVER_LOGGER.debug(f'Парсинг сообщения от клиента: {message}')
 
 
 def main():
@@ -96,25 +120,79 @@ def main():
 
     s = socket(AF_INET, SOCK_STREAM)
     s.bind((listen_address, listen_port))
+    s.settimeout(1)
     SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
                        f'адрес с которого принимаются подключения: {listen_address}. ')
     s.listen(5)
 
+    # список клиентов
+    clients = []
+    # список сообщений
+    messages = []
+
     while True:
-        client, addr = s.accept()
-        SERVER_LOGGER.info(f'Установлено соедение с ПК {addr}')
         try:
-            msg_from_client = get_message(client)
-            SERVER_LOGGER.debug(f'Получено сообщение {msg_from_client}')
-            response = client_response_check(msg_from_client)
-            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
-            create_client_presence(response, client)
-            SERVER_LOGGER.debug(f'Соединение с клиентом {addr} закрыто.')
-            client.close()
-        except json.JSONDecodeError:
-            SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от '
-                                f'клиента {addr}. Соединение закрывается.')
-            client.close()
+            client, addr = s.accept()
+        except OSError:
+            pass
+        else:
+            SERVER_LOGGER.info(f'Установлено соедение с ПК {addr}')
+            clients.append(client)
+
+        # Список на прием сообщений
+        recv_data_lst = []
+        # Список на отправку сообщений
+        send_data_lst = []
+        # Error lst
+        error_lst = []
+
+        # Проверка наличия клиентов
+        try:
+            if clients:
+                recv_data_lst, send_data_lst, error_lst = select.select(clients, clients, [], 0)
+        except OSError:
+            pass
+
+        # Прием сообщения
+        if recv_data_lst:
+            for client_message in recv_data_lst:
+                try:
+                    msg_from_client = get_message(client)
+                    SERVER_LOGGER.debug(f'Получено сообщение {msg_from_client}')
+                    response = client_response_check(msg_from_client, messages)
+                except:
+                    SERVER_LOGGER.info(f'Клиент {client_message.getpeername()} '
+                                       f'отключился от сервера.')
+                    clients.remove(client_message)
+
+        #  Отправка сообщения
+        if messages and send_data_lst:
+            message = {
+                'action': "message",
+                'sender': messages[0][0],
+                'time': time.time(),
+                'message_text': messages[0][1]
+            }
+            del message[0]
+            for waiting_client in send_data_lst:
+                try:
+                    create_client_presence(message, waiting_client)
+                except:
+                    SERVER_LOGGER.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                    clients.remove(waiting_client)
+
+        # try:
+        #     msg_from_client = get_message(client)
+        #     SERVER_LOGGER.debug(f'Получено сообщение {msg_from_client}')
+        #     response = client_response_check(msg_from_client)
+        #     SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
+        #     create_client_presence(response, client)
+        #     SERVER_LOGGER.debug(f'Соединение с клиентом {addr} закрыто.')
+        #     client.close()
+        # except json.JSONDecodeError:
+        #     SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от '
+        #                         f'клиента {addr}. Соединение закрывается.')
+        #     client.close()
 
 
 if __name__ == '__main__':
