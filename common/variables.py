3d8ea51d40ebf095a5287bd742e78fd5f89fa1cf
Lase <lase_star@mail.ru>
2023-04-17 19:38:17 +0300

    Add_dz_14

--- common/variables.py
+++ common/variables.py
@@ -1,7 +1,7 @@
 import logging
 
 # Порт поумолчанию для сетевого ваимодействия
-DEFAULT_PORT = 70000
+DEFAULT_PORT = 60000
 # IP адрес по умолчанию для подключения клиента
 DEFAULT_IP_ADDRESS = '127.0.0.1'
 # Максимальная очередь подключений
@@ -22,6 +22,8 @@ USER = 'user'
 ACCOUNT_NAME = 'account_name'
 SENDER = 'from'
 DESTINATION = 'to'
+DATA = 'bin'
+PUBLIC_KEY = 'pubkey'
 
 # Прочие ключи, используемые в протоколе
 PRESENCE = 'presence'
@@ -35,6 +37,7 @@ LIST_INFO = 'data_list'
 REMOVE_CONTACT = 'remove'
 ADD_CONTACT = 'add'
 USERS_REQUEST = 'get_users'
+PUBLIC_KEY_REQUEST = 'pubkey_need'
 
 # Словари - ответы:
 # 200
@@ -48,4 +51,13 @@ RESPONSE_400 = {
             RESPONSE: 400,
             ERROR: None
         }
+# 205
+RESPONSE_205 = {
+    RESPONSE: 205
+}
 
+# 511
+RESPONSE_511 = {
+    RESPONSE: 511,
+    DATA: None
+}
